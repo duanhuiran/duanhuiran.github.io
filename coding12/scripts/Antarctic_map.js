4916114fd2b556045264f6dcff0059bf8a5f6ea3
DuanHuiran <570989207@qq.com>
2019-01-05 21:23:17 +0800

    Final Project01
    Final Project01

---
+++ coding12/scripts/Antarctic_map.js
@@ -0,0 +1,117 @@
+"use strict";
+var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip");
+
+var mapsvg = d3.select("#mapsvg");
+
+var g_Facilities = mapsvg.append("g").attr("class","Facilities");
+
+var r = 4;
+
+var projection = d3.geoMercator()
+    .scale(492)
+    .rotate([0,90,0])
+    .translate([432,219]);
+
+var geoGenerator = d3.geoPath()
+    .projection(projection);
+
+queue()
+    .defer(d3.json, "dist/Seamask_medium_res_polygon.json")
+    .defer(d3.csv, "dist/COMNAP_Antarctic_Facilities_Master.csv", typeAndSet)
+    .await(loaded);
+
+function typeAndSet(d){
+    d.lon_dd = + d.lon_dd;
+    d.lat_dd = + d.lat_dd;
+    return d;
+}
+
+function loaded(error, Antarctic, Facilities){
+    if(error) throw error;
+
+    // console.log(Facilities);
+
+    var circles = g_Facilities.selectAll("circle")
+        .data(Facilities)
+        .enter()
+        .append("circle");
+
+    circles
+        .attr("cx", function(d){ return projection([d.lon_dd, d.lat_dd])[0]} )
+        .attr("cy", function(d){ return projection([d.lon_dd, d.lat_dd])[1]})
+        .attr("r", r)
+        .attr("fill","rgba(251, 54, 29, 1")
+        .attr("opacity",0)
+        .attr("stroke","#FFF")
+        .attr("stroke-width", .5)
+        .attr("class",function(d){return d.interid});
+
+    circles
+        .on("mouseover", mouseoverFunc) // see below...
+        .on("mousemove", mousemoveFunc) // see below...
+        .on("mouseout", mouseoutFunc); // see below...
+
+    //tooltip制作
+    function mouseoverFunc(d) {
+            console.log(d);
+            tooltip
+                .style("display", null) // 区别"none": 不呈现；"null": 取消之前所有给display的属性。
+                .html("还不知道说啥");
+
+            d3.select(this)
+                .attr("r", r * 2);
+        }
+
+    function mousemoveFunc(d) {
+            // console.log("events", window.event, d3.event);
+            tooltip
+                .style("top", (d3.event.pageY - 10) + "px" )
+                .style("left", (d3.event.pageX + 10) + "px");
+        }
+
+    function mouseoutFunc(d) {
+            tooltip.style("display", "none"); 
+            d3.selectAll(".circle")
+              .attr("r", r);
+        }
+
+
+
+    var i = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: "onEnter"
+        }
+    })
+      , c = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: "onCenter"
+        }
+    })
+      , f = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: "onLeave"
+        }
+    });
+
+    var a=d3.selectAll(".Facilities circle");
+    console.log(a);
+    console.log(a._groups[0]);
+    Array.from(a._groups[0]).forEach(function(t){
+        return e(c,t);
+    });
+    function e(e, t) {
+        // e为触发器，t为每一个圆圈
+        var n = t.className.animVal.slice(0,4)
+          , o = document.getElementById("text" + n)
+        // console.log(n);
+        new ScrollMagic.Scene({
+            triggerElement: "#text" + n,
+            duration:o.getBoundingClientRect().height + document.documentElement.clientHeight
+            }).addTo(e).on("progress",function(e){
+                // console.log(e),
+                t.style.opacity= 0 == e.progress ? 0 : e.progress < 1 ? 1 : 1;
+            })        
+      }
+    }
